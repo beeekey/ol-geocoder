32825d7e06ddd81b22c60dcfe3e8af7fddba5edb
ben <beeekey@users.noreply.github.com>
2023-05-10 11:33:57 +0200

    tried to fix proj4

--- src/nominatim.js
+++ src/nominatim.js
@@ -137,6 +137,9 @@ export class Nominatim {
         'EPSG:2056',
         '+proj=somerc +lat_0=46.95240555555556 +lon_0=7.439583333333333 +k_0=1 +x_0=2600000 +y_0=1200000 +ellps=bessel +towgs84=674.374,15.056,405.346,0,0,0,0 +units=m +no_defs'
       ],
+      [
+        'EPSG:4326',
+        '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees']
     ]);
 
 
